db4dd1b916fe4f276a242b0df8eb17c176c883c9
Saxon Du Plooy <saxonjude@gmail.com>
2020-11-18 15:09:09 +0000

    GSAP done, project text next

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import {gsap, TimelineLite, TweenMax, Power3} from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger'
+
 import Header from './Header';
 import Footer from './Footer';
+import Intro from './Intro';
 import Project from './Project';
 
 import './styling/styles.scss';
 import landingStyles from './styling/landing.module.scss';
-
 import logoPink from './images/logoPink.svg';
 import img1 from './images/img1.jpg';
 import img2 from './images/img2.jpg';
@@ -16,6 +19,8 @@ import img6 from './images/img6.jpg';
 import upperWave from './images/upperWave.svg';
 import lowerWave from './images/lowerWave.svg';
 
+gsap.registerPlugin(ScrollTrigger);
+
 const App = () => {
     const [formState, setFormState] = useState({
         name: "",
@@ -24,6 +29,160 @@ const App = () => {
     });
     const [ success, setSuccess ] = useState(false);
     const [ failure, setFailure ] = useState(false);
+    let mainLogo = useRef(null);
+    let section = useRef(null);
+    let hero = useRef(null);
+    let projects = useRef(null);
+    let projectList = useRef(null);
+    let about = useRef(null);
+    let skills = useRef(null);
+    let form = useRef(null);
+    let tl = new TimelineLite({delay: 6.2});
+
+    useEffect(() => {
+        const heroImg = mainLogo.children[0];
+        const heroSaxon = mainLogo.children[1].children[0];
+        const heroDesc = mainLogo.children[2].children[0];
+        const projectH5 = projects.firstElementChild.children[0];
+        const projectH4 = projects.firstElementChild.children[1];
+
+        const project1 = projectList.children[0];
+        const project2 = projectList.children[1];
+        const project3 = projectList.children[2];
+        const project4 = projectList.children[3];
+        const project5 = projectList.children[4];
+        const project6 = projectList.children[5];
+
+        const aboutHeader = about.children[0];
+        const aboutP1 = about.children[1];
+        const aboutP2 = about.children[2];
+        const aboutP3 = about.children[3];
+        
+        const skillsHeader = skills.children[0];
+        const skills1 = skills.children[1].children[0];
+        const skills2 = skills.children[1].children[1];
+        const skills3 = skills.children[1].children[2];
+        const skills4 = skills.children[1].children[3];
+
+        const projectArray = [project1, project2, project3, project4, project5, project6];
+        const projectTitle = [projectH5, projectH4];
+        const aboutArray = [aboutHeader, aboutP1, aboutP2, aboutP3];
+        const skillsArray = [skillsHeader, skills1, skills2, skills3, skills4];
+
+        TweenMax.to(hero, 0, {css: {visibility: 'visible'}});
+
+        tl.staggerFrom([heroImg, heroSaxon, heroDesc], 1, {
+            y: 90,
+            opacity: 0, 
+            ease: Power3.easeOut, 
+            delay: .2 
+        }, .15, 'hero')
+
+        // HERO LOGO
+
+        projectTitle.forEach((el, index) => {
+            gsap.fromTo(el, {
+                opacity: 0,
+                y: 100,
+            }, {
+                y: 0,
+                duration: .5,
+                opacity: 1,
+                ease: 'none',
+                scrollTrigger: {
+                    id: `section-${index+1}`,
+                    trigger: projectH4,
+                    start: 'top 90%',
+                    toggleActions: 'play none none none',
+                    // markers: true
+                }
+            });
+        })
+
+        // PROJECTS
+
+        projectArray.forEach((el, index) => {
+            gsap.fromTo(el, {
+                opacity: 0,
+                y: 80,
+            }, {
+                y: 0,
+                duration: .3,
+                opacity: 1,
+                ease: 'none',
+                scrollTrigger: {
+                    id: `section-${index+1}`,
+                    trigger: el,
+                    start: 'top 80%',
+                    toggleActions: 'play none none reverse',
+                    // markers: true
+                }
+            });
+        })
+        
+        // ABOUT SECTION
+
+        aboutArray.forEach((el, index) => {
+            gsap.fromTo(el, {
+                opacity: 0,
+                y: 100,
+            }, {
+                y: 0,
+                duration: .4,
+                opacity: 1,
+                ease: 'none',
+                scrollTrigger: {
+                    id: `section-${index+1}`,
+                    trigger: el,
+                    start: 'top 80%',
+                    toggleActions: 'play none none none',
+                    // markers: true
+                }
+            });
+        })
+
+        // SKILLS SECTION
+
+        skillsArray.forEach((el, index) => {
+            gsap.fromTo(el, {
+                opacity: 0,
+                y: 100,
+            }, {
+                y: 0,
+                duration: .4,
+                opacity: 1,
+                ease: 'none',
+                scrollTrigger: {
+                    id: `section-${index+1}`,
+                    trigger: el,
+                    start: 'center 80%',
+                    delay: 1,
+                    toggleActions: 'play none none none',
+                    // markers: true
+                }
+            });
+        })
+
+        // FORM
+
+        gsap.fromTo(form, {
+            opacity: 0,
+            y: 100,
+        }, {
+            y: 0,
+            duration: .4,
+            opacity: 1,
+            ease: 'none',
+            scrollTrigger: {
+                trigger: form,
+                start: 'top 80%',
+                toggleActions: 'play none none none',
+                // markers: true
+            }
+        });
+        tl.from(section, 1, {y: 20, opacity: 0, ease: Power3.easeOut}, 1)
+
+    }, [tl]);
 
     const handleChange = e => {
         setFormState({
@@ -56,15 +215,20 @@ const App = () => {
 
     return (
         <div>
+            <Intro />
             <Header />
-                <div className={landingStyles.hero}>
+                <div ref={el => hero = el} className={landingStyles.hero}>
                     <div className={landingStyles.inner_hero}>
-                        <div className={landingStyles.inner_hero_container}>
+                        <div ref={el => mainLogo = el} className={landingStyles.inner_hero_container}>
                             <img src={logoPink} alt="Pink Logo"/>
-                            <h1>Saxon Du Plooy</h1>
-                            <h2>React Based Front-end Web Developer</h2>
+                            <div className={landingStyles.inner_hero_line}>
+                                <h1>Saxon Du Plooy</h1>
+                            </div>
+                            <div className={landingStyles.inner_hero_line}>
+                                <h2>React Based Front-end Web Developer</h2>
+                            </div>
                         </div>
-                        <section>
+                        <section ref={el => section = el}>
                             <a href="#projects">Projects</a><span>/</span>
                             <a href="#skills">Skills</a><span>/</span>
                             <a href="#contact">Contact</a>
@@ -72,12 +236,12 @@ const App = () => {
                     </div>
                 </div>
                 <div id='projects' className={landingStyles.workList_container}>
-                    <div className={landingStyles.workList_container_inner}>
+                    <div ref={el => projects = el} className={landingStyles.workList_container_inner}>
                         <div className={landingStyles.workList_container_heading}>
                             <h4>Case Studies</h4>
                             <h3>Featured Work</h3>
                         </div>
-                        <div className={landingStyles.workList_container_projects}>
+                        <div ref={el => projectList = el} className={landingStyles.workList_container_projects}>
                             <Project
                              img={img1}
                              title='StreamMe'
@@ -89,54 +253,54 @@ const App = () => {
                              skill6='Axios'
                              skill7='Git'
                              skill8='CSS/SCSS'
-                             url1=''
-                             url2=''
-                             url3=''
+                             url1='http://www.streamme.world/'
+                             url2='https://github.com/SaxonJude/StreamMe'
+                             url3='https://dribbble.com/shots/14329752-StreamMe-TMDB-desktop-ui-layout'
                              dribble='true'
                              />
                             <Project
                              img={img2}
                              title='AndyTud'
                              skill1='React'
-                             skill2='Redux'
-                             skill3='TMDB API'
-                             skill4='MongoDB'
-                             skill5='Node.Js'
-                             skill6='Axios'
+                             skill2='Gatsby'
+                             skill3='Bootstrap'
+                             skill4='GSAP'
+                             skill5='JS ES6'
+                             skill6='CSS/SCSS'
                              skill7='Git'
-                             skill8='CSS/SCSS'
-                             url1=''
-                             url2=''
-                             url3=''
+                             skill8='Node.Js'
+                             url1='https://5fb3b9883debc3000709ae1b--focused-almeida-f5f7fa.netlify.app/'
+                             url2='https://github.com/SaxonJude/andrew-tud'
+                             url3='https://dribbble.com/shots/14590009-Personal-Trainer-Website'
                              dribble='true'
                              />
                             <Project
                              img={img3}
                              title='Nexia'
                              skill1='React'
-                             skill2='Redux'
-                             skill3='TMDB API'
-                             skill4='MongoDB'
-                             skill5='Node.Js'
-                             skill6='Axios'
+                             skill2='Gatsby'
+                             skill3='Boostrap'
+                             skill4='GraphQL'
+                             skill5='HTML5'
+                             skill6='CSS/SCSS'
                              skill7='Git'
-                             skill8='CSS/SCSS'
-                             url1=''
-                             url2=''
-                             url3=''
+                             skill8=''
+                             url1='https://5fb2ce6f14e7150007964ba2--jovial-edison-56bea8.netlify.app/'
+                             url2='https://github.com/SaxonJude/Nexia'
+                             url3='https://dribbble.com/shots/14501070-Nexia-Ecom-Website'
                              dribble='true'
                              />
                             <Project
                              img={img4}
                              title='Portfolio'
                              skill1='React'
-                             skill2='Redux'
-                             skill3='TMDB API'
-                             skill4='MongoDB'
+                             skill2='HTML5'
+                             skill3='CSS/SCSS'
+                             skill4='Git'
                              skill5='Node.Js'
-                             skill6='Axios'
-                             skill7='Git'
-                             skill8='CSS/SCSS'
+                             skill6=''
+                             skill7=''
+                             skill8=''
                              url1=''
                              url2=''
                              url3=''
@@ -145,31 +309,31 @@ const App = () => {
                             <Project
                              img={img5}
                              title='Forkify'
-                             skill1='React'
-                             skill2='Redux'
-                             skill3='TMDB API'
-                             skill4='MongoDB'
-                             skill5='Node.Js'
-                             skill6='Axios'
-                             skill7='Git'
-                             skill8='CSS/SCSS'
-                             url1=''
-                             url2=''
+                             skill1='food2Fork API'
+                             skill2='Babel'
+                             skill3='Webpack'
+                             skill4='JS ES6'
+                             skill5='Axios'
+                             skill6='Git'
+                             skill7='HTML5'
+                             skill8='CSS3'
+                             url1='https://5fb28fc87c56b6000889e936--frosty-lovelace-145715.netlify.app/'
+                             url2='https://github.com/SaxonJude/Forkify-Updated'
                              url3=''
                              />
                             <Project
                              img={img6}
                              title='ToDo List'
-                             skill1='React'
-                             skill2='Redux'
-                             skill3='TMDB API'
-                             skill4='MongoDB'
-                             skill5='Node.Js'
-                             skill6='Axios'
-                             skill7='Git'
-                             skill8='CSS/SCSS'
-                             url1=''
-                             url2=''
+                             skill1='jQuery'
+                             skill2='JS Es6'
+                             skill3='HTML5'
+                             skill4='CSS3'
+                             skill5='Git'
+                             skill6=''
+                             skill7=''
+                             skill8=''
+                             url1='https://5fb292857c56b600088b15f6--vibrant-lalande-5c24dc.netlify.app/'
+                             url2='https://github.com/SaxonJude/todo-jQuery'
                              url3=''
                              />
                         </div>
@@ -178,11 +342,13 @@ const App = () => {
                 <div className={landingStyles.content_container}>
                     <img src={upperWave} alt="Wave"/>
                     <div className={landingStyles.content_box}>
-                        <div className={landingStyles.content_about}>
+                        <div ref={el => about = el} className={landingStyles.content_about}>
                             <h4>Who Am I<span>?</span></h4>
                             <p>
-                                Hi, I’m <span>Saxon Du Plooy</span>, I was born in <span>Durban, SA</span> and moved to the UK when I was 3. 
-                                I’m a self-taught front end web developer with a strong <span>passion</span> for web development I’m constantly learning new skills to grow my knowledge. My experience includes from building beautiful static CSS/JavaScript websites, to developing React/Redux applications that require authentication.
+                            Hello, I’m <span>Saxon Du Plooy</span>, born in <span>Durban, SA</span> and re-located to the New Forest when was 3.  I’m a self-taught front end web developer with a strong <span>passion</span> for web development and constantly learning new skills to grow my knowledge. 
+                            </p>
+                            <p>
+                                My experience includes building beautiful static CSS3/JavaScript websites, to developing complex React/Redux applications that require authentication. I’ve also done some freelanced <span>Wordpress</span> sites on the side.
                             </p>
                             <p id='skills'>
                                 I've dedicated my time to better myself as a developer and a problem 
@@ -190,7 +356,7 @@ const App = () => {
                             </p>
                         </div>
                         <hr />
-                        <div className={landingStyles.content_skills}>
+                        <div ref={el => skills = el} className={landingStyles.content_skills}>
                             <h4>Skills<span>.</span></h4>
                             <ul>
                                 <li>
@@ -212,7 +378,7 @@ const App = () => {
                             </ul>
                         </div>
                         <hr />
-                        <div className={landingStyles.content_form}>
+                        <div ref={el => form = el} className={landingStyles.content_form}>
                             <h4>Get in touch <span>!</span></h4>
                             <form onSubmit={handleSubmit} name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
                                 <label htmlFor="name">Name <span>*</span></label>
@@ -221,7 +387,9 @@ const App = () => {
                                 <input id="email" name="email" type="email" onChange={handleChange} value={formState.email} />
                                 <label htmlFor="message">Message <span>*</span></label>
                                 <input id="message" name="message" type="text" onChange={handleChange} value={formState.message} />
-                                <button className={success && !failure ? landingStyles.success : ''} type="submit">{success && !failure ? 'Sent!' : 'Send Message'}</button>
+                                <div className={landingStyles.btn_container}>
+                                    <button className={`${success && !failure ? landingStyles.success : ''} ${landingStyles.btn} ${landingStyles.btn1}`} type="submit">{success && !failure ? 'Sent!' : 'Send Message'}</button>
+                                </div>
                             </form>
                         </div>
                     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,36 @@
 import React, { useState, useEffect } from 'react';
-import headerStyles from './styling/header.module.scss';
 
+import headerStyles from './styling/header.module.scss';
 import logoWhite from './images/logoWhite.svg';
+
+function useWindowSize() {    
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+
+    useEffect(() => {
+      function handleResize() {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+      
+      window.addEventListener("resize", handleResize);
+      
+      handleResize();
+      
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); 
+  
+    return windowSize;
+}
 
 const Header = () => {
     const [scroll, setScroll] = useState(false);
+    const [show, setShow] = useState(false);
+    const size = useWindowSize();
 
     useEffect(() => {
         if(typeof window !== 'undefined') {
@@ -24,14 +50,19 @@ const Header = () => {
     return (
         <nav className={headerStyles.header}>
             <div className={headerStyles.inner_header}>
-                <img src={logoWhite} alt="white header logo"/>
-                <ul className={headerStyles.header_anchors}>
+                <a href="#"><img src={logoWhite} alt="white header logo"/></a>
+                <button onClick={() => setShow(!show)} className={headerStyles.hamburger}>
+                    <i className="fas fa-bars"></i>
+                </button>
+                <ul className={`${show && size.width < 620 ? headerStyles.show : headerStyles.header_anchors}`}>
                     <li><a href='#projects' rel="noopener noreferrer">Projects</a></li>
                     <li><a href='#skills'rel="noopener noreferrer">Skills</a></li>
                     <li><a href='#contact' rel="noopener noreferrer">Contact</a></li>
                 </ul>
             </div>
-            <a href="#"><i className={`fas fa-long-arrow-alt-up ${scroll ? headerStyles.active : ''}`}></i></a>
+            <a href="#contact">
+                <i className={`${headerStyles.message} fas fa-comment-alt ${scroll ? headerStyles.active : ''}`}></i>
+            </a>
         </nav>
     );
 }
